1fccd2acf11bf0f7a34e345666c92664bf506642
KyanPHP <128573943+KyanPHP@users.noreply.github.com>
2023-03-25 12:27:19 +1100

    Update ResetTask.php

--- AutoMineReset/src/auto/ResetTask.php
+++ AutoMineReset/src/auto/ResetTask.php
@@ -3,17 +3,17 @@
 namespace auto;
 
 use auto\Main;
-
 use pocketmine\scheduler\Task;
-use pocketmine\Player;
 
-class ResetTask extends Task{
+class ResetTask extends Task {
   
-  public function __construct(Main $main){
+  private $main;
+  
+  public function __construct(Main $main) {
     $this->main = $main;
   }
   
-  public function onRun(): void{
+  public function onRun(): void {
     $this->main->resetmine();
   }
 }
